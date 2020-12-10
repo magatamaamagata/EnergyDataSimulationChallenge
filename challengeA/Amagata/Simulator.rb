cc6297cdb5f88e4b612e031e73cc5ba3533ffd03
magatamaamagata <magatamaamagata@gmail.com>
2020-12-10 19:15:53 +0900

    Update Simulator.rb

--- challengeA/Amagata/Simulator.rb
+++ challengeA/Amagata/Simulator.rb
@@ -10,17 +10,25 @@ class Calculator
     info=Information.new
     # informationクラスのゲッターを配列に入れる。（手動）
     arrays_all=[info.tepco,info.loooop,info.tokyo_gas]  # 会社を追加した場合ここに新料金を足さなければならない。,info.amagata
-    price=[]
+    @prices=[]
     # 条件から各社料金を計算
-    arrays_all.each do |array|
+    arrays_all.map.with_index do |array,i|
+      # 配列の中身の情報
       if array[:contract_current].include?(@ampare)
-        # 基本料金を定義 アンペアがbasic_chargeにあればその基本料金料金、なければ0
+        # 基本料金を定義 アンペアがbasic_chargeにあればその基本料金
         if info.basic_charge[:"#{@ampare}A"]
           basic = info.basic_charge[:"#{@ampare}A"]
         else
+          # 基本料金がないプラン用
           basic = 0
         end
+
         # 単価決定の条件式1 
+        # range1= array[:range][0]
+        # range2= array[:range][1]
+        # pay_per_use1 =array[:pay_per_use][0]
+        # pay_per_use2 =array[:pay_per_use][1]
+        # pay_per_use3 =array[:pay_per_use][2]
         if array[:range][0]<= @kilowatt_hour && @kilowatt_hour < array[:range][1]
           # 料金=基本料金+従量課金
           sum = basic + array[:pay_per_use][0] * @kilowatt_hour
@@ -31,23 +39,22 @@ class Calculator
         else
           sum = basic + array[:pay_per_use][2] * @kilowatt_hour
         end
-      else
-        sum = 0
       end
-      price << sum
+      if sum != nil
+        @prices << { provider_name: "#{arrays_all[i][:provider_name]}", plan_name: "#{arrays_all[i][:plan_name]}", price: sum }
+      end
     end
 
     # 料金の計算結果の個数だけ会社とプラント料金を、配列@resultsに入れる。
-    @results=[]
-    price.length.times do |i|
-      if price[i] != 0
-        @results << { provider_name: "#{arrays_all[i][:provider_name]}", plan_name: "#{arrays_all[i][:plan_name]}", price: "#{price[i]}" }
-      end
-    end
+    # @prices= @prices.map.with_index do |price, i|
+    #   if price != nil
+    #   { provider_name: "#{arrays_all[i][:provider_name]}", plan_name: "#{arrays_all[i][:plan_name]}", price: price }
+    #   end
+    # end
   end
   # 結果ゲッター
   def results
-    @results
+    @prices
   end
 end
 
@@ -62,7 +69,11 @@ class Information
       "30A": 858,
       "40A": 1144,
       "50A": 1430,
-      "60A": 1716 
+      "60A": 1716,
+      # kVA契約:{
+        # "10A": 286}
+        # },# 60A以上は契約プランが変わるが10Aにつき286円
+      # 低圧電力: 1122
     }
   end
 
@@ -74,7 +85,7 @@ class Information
       "plan_name": "従量電灯B",
       "range": [0,120,300],
       "pay_per_use": [19.88,26.48,30.57],
-      "contract_current": [30,40,50,60]
+      "contract_current": [10,15,20,30,40,50,60]
     }
   end
 
@@ -84,7 +95,7 @@ class Information
       "plan_name": "おうちプラン",
       "range": [0,0,0],
       "pay_per_use": [26.40,26.40,26.40],
-      "contract_current": [*10..60]
+      "contract_current": [10,15,20,30,40,50,60]
     }
   end
 
@@ -94,7 +105,7 @@ class Information
       "plan_name": "ずっとも電気１",
       "range": [0,140,350],
       "pay_per_use": [23.67,23.88,26.41],
-      "contract_current": [10,15,20,30,40,50,60]
+      "contract_current": [30,40,50,60]
     }
   end
 #追加用
